797ef368a261c3d39b775771fc6959d40c8bbbf9
MrMagicPenguin <wstan45@student.monash.edu>
2017-07-17 19:03:53 +0800

    added apple and updates tweaked

---
+++ appleClass.py
@@ -0,0 +1,32 @@
+import pygame
+import random
+
+'''
+Used to split apple to respawn in grids of size - limit
+'''
+def myround(x, limit):
+    remainder = x % limit
+    val1 = limit - remainder
+    return x + val1
+
+class Apple:
+
+    def __init__(self):
+        self.x = 600
+        self.y = 200
+        self.apple_width = 20
+        self.apple_height = 20
+
+    def update(self, display_width,display_height):
+        randx = random.randint(0, display_width-20)        
+        randy = random.randint(0, display_height-20)
+
+        self.x = myround(randx, 20)
+        self.y = myround(randy, 20)
+
+    def show(self, display):
+        pygame.draw.rect(display, (255,0,0), (self.x, self.y, self.apple_width,
+                                              self.apple_height))
+        
+
+    
---
+++ snake.py
@@ -0,0 +1,55 @@
+import pygame
+
+
+def collides(s, a):
+   
+
+    return (s.posX == a.x and s.posY == a.y)
+    
+pygame.init();
+
+display_width = 800;
+display_height = 600;
+gameWindow = pygame.display.set_mode((display_width, display_height));
+
+clock = pygame.time.Clock();
+
+from snakeClass import Snake;
+from appleClass import Apple;
+
+snake = Snake();
+apple = Apple()
+
+gameQuit = False;
+
+while not gameQuit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameQuit = True;
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.update_dir(0, -snake.rect_height);
+            elif event.key == pygame.K_LEFT:
+                snake.update_dir(-snake.rect_width, 0);
+            elif event.key == pygame.K_RIGHT:
+                snake.update_dir(snake.rect_width, 0);
+            elif event.key == pygame.K_DOWN:
+                snake.update_dir(0, snake.rect_height);
+
+    snake.move();
+    snake.constrain(display_width, display_height);
+    
+    # clears the screen
+    gameWindow.fill((0, 0, 0));
+    snake.show(gameWindow);
+    apple.show(gameWindow)
+
+    if collides(snake, apple):
+        apple.update(display_width, display_height)
+    pygame.display.update();
+
+    clock.tick(15);
+
+pygame.quit();
+quit();
